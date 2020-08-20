86075b098f2b514f320a6c745739563992a0debb
lovehacker <61407838+lovehacker404@users.noreply.github.com>
2020-08-20 12:25:12 +0500

    Update run.py

--- run.py
+++ run.py
@@ -1,23 +1,14 @@
 #!/usr/bin/python3
 # coding=utf-8
 
-#######################################################
-# File           : run.py                             #
-# Author         : DulLah                             #
-# Github         : https://github.com/dz-id           #
-# Facebook       : https://www.facebook.com/dulahz    #
-# Telegram       : https://t.me/DulLah                #
-# Python version : 3.8+                               #
-#######################################################
-#         RECODE? OKE CANTUMKAN NAMA PEMBUAT          #
-#######################################################
+############################BlackMafia########################
 
 import shutil, platform
 
 py_version = platform.python_version()
 
 if py_version < '3.7':
-    exit('WARNING anda menggunakan python version %s silahkan upgrade ke 3.7++'%(py_version))
+    exit('WARNING you are using the python version %s please upgrade to 3.7++'%(py_version))
 
 cache = ['src/__pycache__', 'src/data/__pycache__']
 
